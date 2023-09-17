fe83b92a9a3d246a0cbff7aee50cabf1c7a69621
Lucas Alves <lucasalves.12@live.com>
2023-09-17 16:07:17 -0300

    alterar método calcularImposto

--- src/main/java/com/example/estadosprojeto/ValorImposto.java
+++ src/main/java/com/example/estadosprojeto/ValorImposto.java
@@ -1,8 +1,5 @@
 package com.example.estadosprojeto;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class ValorImposto {
     private String estado;
     private float valor;
@@ -38,7 +35,7 @@ public class ValorImposto {
     }
 
     public void setValor(float valor) {
-        if(valor > 0) {
+        if (valor > 0) {
             this.valor = valor;
         } else {
             throw new IllegalArgumentException("O valor deve ser maior do que zero!");
@@ -46,36 +43,25 @@ public class ValorImposto {
     }
 
     public float calcularImposto() {
-        Map<String, Float> aliquotasPorEstado = new HashMap<>();
-        aliquotasPorEstado.put("AC", 0.17f);
-        aliquotasPorEstado.put("AL", 0.17f);
-        aliquotasPorEstado.put("AM", 0.18f);
-        aliquotasPorEstado.put("AP", 0.18f);
-        aliquotasPorEstado.put("BA", 0.18f);
-        aliquotasPorEstado.put("CE", 0.18f);
-        aliquotasPorEstado.put("DF", 0.18f);
-        aliquotasPorEstado.put("ES", 0.17f);
-        aliquotasPorEstado.put("GO", 0.17f);
-        aliquotasPorEstado.put("MA", 0.18f);
-        aliquotasPorEstado.put("MT", 0.17f);
-        aliquotasPorEstado.put("MS", 0.17f);
-        aliquotasPorEstado.put("MG", 0.18f);
-        aliquotasPorEstado.put("PA", 0.17f);
-        aliquotasPorEstado.put("PB", 0.18f);
-        aliquotasPorEstado.put("PR", 0.18f);
-        aliquotasPorEstado.put("PE", 0.18f);
-        aliquotasPorEstado.put("PI", 0.18f);
-        aliquotasPorEstado.put("RN", 0.18f);
-        aliquotasPorEstado.put("RS", 0.18f);
-        aliquotasPorEstado.put("RJ", 0.18f);
-        aliquotasPorEstado.put("RO", 0.175f);
-        aliquotasPorEstado.put("RR", 0.17f);
-        aliquotasPorEstado.put("SC", 0.17f);
-        aliquotasPorEstado.put("SP", 0.18f);
-        aliquotasPorEstado.put("SE", 0.18f);
-        aliquotasPorEstado.put("TO", 0.18f);
+        float aliquota = 0.0f;
 
-        Float aliquota = aliquotasPorEstado.getOrDefault(estado,0.0f);
+        if ("AC".equals(estado) || "AL".equals(estado)) {
+            aliquota = 0.17f;
+        } else if ("AM".equals(estado) || "AP".equals(estado) || "BA".equals(estado) ||
+                "CE".equals(estado) || "DF".equals(estado) || "ES".equals(estado) ||
+                "GO".equals(estado) || "MA".equals(estado) || "MT".equals(estado) ||
+                "MS".equals(estado) || "MG".equals(estado) || "PA".equals(estado) ||
+                "PB".equals(estado) || "PR".equals(estado) || "PE".equals(estado) ||
+                "PI".equals(estado) || "RN".equals(estado) || "RS".equals(estado) ||
+                "RJ".equals(estado)) {
+            aliquota = 0.18f;
+        } else if ("RO".equals(estado)) {
+            aliquota = 0.175f;
+        } else if ("RR".equals(estado) || "SC".equals(estado) || "SE".equals(estado) ||
+                "TO".equals(estado)) {
+            aliquota = 0.17f;
+        }
+
         return valor * aliquota;
     }
 }
